86a3b099d8b37a12094fb4966a9c3774896c5725
Eric <72662742+DoubleXEric@users.noreply.github.com>
2024-06-25 23:54:51 +0200

    fixed sidebar bug and updated colors

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const mainContent = document.getElementById("main");
 
-    if (sidebar.style.width === "0px" || sidebar.style.width === "") {
+    if (sidebar.style.width === "0px") {
         sidebar.style.width = "30%";
         mainContent.style.marginLeft = "30%";
     } else {
