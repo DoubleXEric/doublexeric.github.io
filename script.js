2154c03fad0153c05b8ae0009fcc3810799c5a38
Eric <72662742+DoubleXEric@users.noreply.github.com>
2024-06-26 14:26:44 +0200

    changed color theme

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const mainContent = document.getElementById("main");
+    const mainContentBackground = document.getElementById("mainBackground");
+    const navBar = document.getElementById("navBar");
 
     if (sidebar.style.width === "0px") {
         sidebar.style.width = "30%";
         mainContent.style.marginLeft = "30%";
+        mainContentBackground.style.left = "30%";
     } else {
         sidebar.style.width = "0";
         mainContent.style.marginLeft = "0";
+        mainContentBackground.style.left = "0";
     }
 }
 
 window.onload = function () {
+    const sidebar = document.getElementById("sidebar");
+    const mainContent = document.getElementById("main");
+    const mainContentBackground = document.getElementById("mainBackground");
+    const navBar = document.getElementById("navBar");
+
+    sidebar.style.width = "30%";
+    mainContent.style.marginLeft = "30%";
+    mainContentBackground.style.left = "30%";
+
     var particles = Particles.init({
         selector: '.background',
         maxParticles: 400,
-
-
-// options for breakpoints
-        responsive: [
-            {
-                breakpoint: 768,
-                options: {
-                    maxParticles: 200,
-                    color: '#48F2E3',
-                    connectParticles: true
-                }
-            }
-        ]
+        connectParticles: true,
+        color: ['#FFFEFC', '#666666', '#ba0013']
     });
 };
 
